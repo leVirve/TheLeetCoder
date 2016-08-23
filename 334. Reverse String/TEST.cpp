f7b8970ef392686eaa7b471948828514be81089f
Salas <gae.m.project@gmail.com>
2016-08-23 14:45:15 +0100

    remove perosonal code and config

--- 334. Reverse String/TEST.cpp
+++
@@ -1,9 +0,0 @@
-#include "../framework/header.hpp"
-#include "solution.hpp"
-
-TEST_CASE( "Solution tested", "[reverseString]" ) {
-    Solution s;
-
-    REQUIRE( s.reverseString("ABCD") == "DCBA" );
-    REQUIRE( s.reverseString("ABCDa") != "DCBA" );
-}
