1f55345c2f0fbf8d82537bf2c148f6089009a2bd
Salas <gae.m.project@gmail.com>
2017-07-23 09:50:56 +0800

    cleaner solution for question2

--- 2. Add Two Numbers/solution.hpp
+++ 2. Add Two Numbers/solution.hpp
@@ -9,17 +9,17 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *node = new ListNode(0), *p = node;
+        ListNode node(0), *p = &node;
 
-        while (l1 || l2) {
-            int c = p->val;
+        int c = 0;
+        while (l1 || l2 || c) {
             if (l1) c += l1->val, l1 = l1->next;
             if (l2) c += l2->val, l2 = l2->next;
-            p->val = c % 10;
-            if (!(c / 10) && (!l1 && !l2)) break;
-            p = (p->next = new ListNode(c / 10));
+            p->next = new ListNode(c % 10);
+            p = p->next;
+            c /= 10;
         }
 
-        return node;
+        return node.next;
     }
 };
